c3fbee0a8327fc9986e2314400286680bf2bbb71
Tiago Eloy Possidonio <tiagoeloy@alunos.utfpr.edu.br>
2023-08-24 20:46:28 -0300

    update

--- Lista 02.js
+++ Lista 02.js
@@ -1,15 +1,27 @@
-console.log(Tratamento("a1b2c3d4e5f6"));
+// Exercício 01
 function Tratamento() {
-    str = document.getElementById("InputFrase").value;
-    let vetorText = str.split("");
-    str = "";
-    for(let c=0; c < vetorText.length; c++){
-        if(vetorText[c] == "1" || vetorText[c] == "2" || vetorText[c] == "3" || vetorText[c] == "4" || vetorText[c] == "5" || vetorText[c] == "6" || vetorText[c] == "7" || vetorText[c] == "8" || vetorText[c] == "9" || vetorText[c] == "0") {
-            vetorText[c].push;
-        }
-        else{
-            str += vetorText[c].toUpperCase();
-        }
+  str = document.getElementById("Frase").value;
+  tratado = document.getElementById("FraseTratada");
+  let vetorText = str.split("");
+  str = "";
+  for (let c = 0; c < vetorText.length; c++) {
+    if (
+      vetorText[c] == "1" ||
+      vetorText[c] == "2" ||
+      vetorText[c] == "3" ||
+      vetorText[c] == "4" ||
+      vetorText[c] == "5" ||
+      vetorText[c] == "6" ||
+      vetorText[c] == "7" ||
+      vetorText[c] == "8" ||
+      vetorText[c] == "9" ||
+      vetorText[c] == "0"
+    ) {
+      vetorText[c].push;
+    } else {
+      str += vetorText[c].toUpperCase();
     }
-    return str;
+  }
+  tratado.value = str;
+  console.log(tratado);
 }
