e8deb477a031c17162c665e2ef40948dc05def8a
Tiago Eloy Possidonio <tiagoeloy@alunos.utfpr.edu.br>
2023-10-03 15:13:56 -0300

    Ajustes

--- Aula pratica 02/routes/index.js
+++ Aula pratica 02/routes/index.js
@@ -3,14 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  usuario = {
-    nome: req.session.user
+  if (req.session.user) {
+    var usuario = {};
+    usuario = {
+      nome: req.session.user
+    }
+    res.render("index", { usuario: usuario }); 
+  } else{
+    res.render("index", null); 
   }
-  res.render("index", usuario);
+
 });
 
 router.post('/salvauser', function(req, res) {
-
     const userActive = req.body.username;
     req.session.user = userActive;
     res.redirect("/");
