b829f43ad02c60066fa8f3fe08c14e9f4c3e9e35
Tiago Eloy Possidonio <tiagoeloy@alunos.utfpr.edu.br>
2023-10-04 22:53:35 -0300

    /contador - Crie 2 contadores de acesso diferente, um para todas as requisições recebidas e um para o usuário em questão

--- Aula pratica 02/routes/index.js
+++ Aula pratica 02/routes/index.js
@@ -1,26 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+let contadorGlobalReq = 0;
+let contadorUserReq = {} 
 /* GET home page. */
 router.get('/', function(req, res) {
+  contadorGlobalReq++;
+  let user = req.session.nome;
+  if(user){
+    if(contadorUserReq[user]){
+      contadorUserReq[user]++;
+    } else{
+      contadorUserReq[user] = 1;
+    }
+  }
   var args = {
     nome: req.session.nome
   };
+  console.log(`Req Global: ${contadorGlobalReq} // Req user ${user}: ${contadorUserReq[user]}`)
   res.render("index", args); 
 });
 
 router.post('/salvauser', function(req, res) {
+    contadorGlobalReq++;
+    if(contadorUserReq[req.session.nome]){
+      contadorUserReq[req.session.nome]++;
+    }
     const userActive = req.body.username;
     req.session.nome = userActive;
     res.redirect("/");
 });
 
 router.get('/random/:n', function(req, res){
+  contadorGlobalReq++;
+  if(contadorUserReq[req.session.nome]){
+    contadorUserReq[req.session.nome]++;
+  }
   res.end(`<h1> Numero: ${req.cookies.numero}</h1>`);
 })
 
 
 router.get('/random', (req, res) => {
+    contadorGlobalReq++;
+    if(contadorUserReq[req.session.nome]){
+      contadorUserReq[req.session.nome]++;
+    }
     var number = Math.random() * 1000 * 10000;
     res.cookie("numero", number);
     res.redirect('/random/' + number);
